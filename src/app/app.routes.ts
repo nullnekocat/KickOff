413b1492e8d4f32cd688e8e8cc3e9676d79b2444
Rey_礼 <142746569+ReyNekoCat@users.noreply.github.com>
2025-09-18 17:27:37 -0600

    feat: create simulator page prototype

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './Pages/home/home';
 import { Mensajes } from './Pages/mensajes/mensajes';
+import { Simulador } from './Components/simulador/simulador';
 
 
 export const routes: Routes = [
-    { path: '', component: Home },   // Home es la página inicial
-    { path: 'mensajes', component: Mensajes },
-    { path: '**', redirectTo: '' }
+  { path: '', component: Home },   // Home es la página inicial
+  { path: 'mensajes', component: Mensajes },
+  { path: 'simulador', component: Simulador },
+  { path: '**', redirectTo: '' }
 ];
 
 
