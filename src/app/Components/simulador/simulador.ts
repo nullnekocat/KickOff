a457b41c8e416959e2e0592b5beea32f7f6d4300
Rey_礼 <142746569+ReyNekoCat@users.noreply.github.com>
2025-09-18 13:51:47 -0600

    first sim commit

---
+++ src/app/Components/simulador/simulador.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulador',
+  imports: [],
+  templateUrl: './simulador.html',
+  styleUrl: './simulador.css'
+})
+export class Simulador {
+
+}
