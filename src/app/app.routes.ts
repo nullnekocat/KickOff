8d4766784497bb064fafe93f4b7608f8b7b253ee
Rey_礼 <142746569+ReyNekoCat@users.noreply.github.com>
2025-09-18 17:41:39 -0600

    feat: create ranking page prototype

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './Pages/home/home';
 import { Mensajes } from './Pages/mensajes/mensajes';
 import { Simulador } from './Components/simulador/simulador';
+import { Ranking } from './Components/ranking/ranking';
 
 
 export const routes: Routes = [
   { path: '', component: Home },   // Home es la página inicial
   { path: 'mensajes', component: Mensajes },
   { path: 'simulador', component: Simulador },
+  { path: 'ranking', component: Ranking },
   { path: '**', redirectTo: '' }
 ];
 
