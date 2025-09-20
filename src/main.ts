12b80a28c97307efd6eda95014f9cb466eb616a4
Rey_礼 <142746569+ReyNekoCat@users.noreply.github.com>
2025-09-19 23:02:14 -0600

    style: try flag emoji to work

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+polyfillCountryFlagEmojis();
