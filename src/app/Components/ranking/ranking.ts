05def549700a49dd0f13a49bd13a20d9356c92d0
Areli Sarai <arelifranco234@gmail.com>
2025-10-20 18:01:40 -0600

    chore: change styles, change log out and update log out structure

--- src/app/Components/ranking/ranking.ts
+++ src/app/Components/ranking/ranking.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { Menu } from '../menu/menu';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, Menu],
   templateUrl: './ranking.html',
   styleUrl: './ranking.css'
 })
